d3a9f777c00f01de4f366c24d552470dfaa29667
Eccenux <eccenux+git@gmail.com>
2018-06-19 01:34:21 +0200

    not working on mobile :-/
    add logging and more @match/include

--- map-enlarge.meta.js
+++ map-enlarge.meta.js
@@ -1,9 +1,12 @@
 ﻿// ==UserScript==
 // @name         Map enlarge
 // @namespace    pl.enux.ingressmosaik
-// @version      0.0.1
-// @description  [0.0.1] Makes the map larger when you open Map tab in new tab.
+// @version      0.0.2
+// @description  [0.0.2] Makes the map larger when you open Map tab in new tab.
 // @author       Maciej Nux Jaros
+// @include      https://*.ingressmosaik.com/*
+// @include      https://ingressmosaik.com/*
+// @match        https://*.ingressmosaik.com/*
 // @match        https://ingressmosaik.com/*
 // @grant        none
 // ==/UserScript==
--- map-enlarge.user.js
+++ map-enlarge.user.js
@@ -1,9 +1,12 @@
 // ==UserScript==
 // @name         Map enlarge
 // @namespace    pl.enux.ingressmosaik
-// @version      0.0.1
-// @description  [0.0.1] Makes the map larger when you open Map tab in new tab.
+// @version      0.0.2
+// @description  [0.0.2] Makes the map larger when you open Map tab in new tab.
 // @author       Maciej Nux Jaros
+// @include      https://*.ingressmosaik.com/*
+// @include      https://ingressmosaik.com/*
+// @match        https://*.ingressmosaik.com/*
 // @match        https://ingressmosaik.com/*
 // @grant        none
 // @updateURL    https://github.com/Eccenux/ingressmosaik-plugin-map-enlarge/raw/master/map-enlarge.meta.js
@@ -12,17 +15,16 @@
 
 (function($) {
     'use strict';
+	
+	console.log('Hello from TM script');
+	console.log('hash: ', location.hash);
+	
     if (location.hash === '#maps') {
         // collapse everything but the tabs
         infoHide('1');
+		// show maps tab
         setTimeout(function(){
             $("#ib2 a[href='#maps']").click(); // this has 300 ms timeout inside
-        }, 100)
-        // will not work -- "Failed to execute 'requestFullscreen' on 'Element': API can only be initiated by a user gesture."
-        /*
-        setTimeout(function(){
-            mymap.toggleFullscreen();
-        }, 1000);
-        */
+        }, 100);
     }
 })(jQuery);
